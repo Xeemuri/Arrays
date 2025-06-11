be739f482e25eb978da74f20be5a9bda76e3ccd8
Илья Щербаков <iliya309@mail.ru>
2025-06-11 18:11:33 +1000

    Hexadecimal DONE

--- Numerics/main.cpp
+++ Numerics/main.cpp
@@ -1,33 +1,42 @@
 ﻿#include <iostream>
 using namespace std;
 
+//#define BINARY
 void main()
 {
 	setlocale(LC_ALL, "");
-	int n;
-	cout << "Введите число в десятичной системе счисления: "; cin >> n;
-	int n1 = n;
+	int decimal;
+	cout << "Введите число в десятичной системе счисления: "; cin >> decimal;
+#ifdef BINARY
+	int n1 = decimal;
 	const int SIZE = 32;
 	int bin[SIZE], hex[SIZE];
-	int counter = 0;
-	int remainder = 0;
+	int i = 0;
 
-	while (n != 0)
+	/*while (decimal != 0)
 	{
-		remainder = n % 2;
+		remainder = decimal % 2;
 		bin[counter++] = remainder;
-		n /= 2;
-	}
+		decimal /= 2;
+	}*/
+	for (; decimal; decimal /= 2) bin[i++] = decimal % 2;
 
 	cout << "Число в двоичной системе: ";
-	for (int i = (counter - 1); i >= 0; i--)
-	{
-		cout << bin[i];
-	}
+	for (--i; i >= 0; i--) cout << bin[i];
 	cout << endl;
+#endif // BINARY
 
-	counter = 0;
-	remainder = 0;
+	const int  MAX_HEX_CAPACITY = 8;
+	char hex[MAX_HEX_CAPACITY] = {};
+
+	int i = 0;
+	for (; decimal; decimal /= 16) {
+		hex[i++] = decimal % 16 < 10 ? decimal % 16 + 48 : decimal % 16 + 55;
+		/*hex[i] = decimal % 16;
+		hex[i++] += hex[i] < 10 ? hex[i] + 48 : hex[i] + 55;*/
+	}
+	for (--i; i > 0; i--)cout << hex[i];
+	cout << endl;
 
 	/*while (n1 != 0)
 	{
@@ -45,8 +54,8 @@ void main()
 		n1 /= 16;
 
 	}*/
-	while (n1 != 0) {remainder = n1 % 16; hex[counter++] = (remainder < 10) ? remainder + '0' : 'A' + remainder - 10; n /= 16;}
+	/*while (n1 != 0) { remainder = n1 % 16; hex[i++] = (remainder < 10) ? remainder + '0' : 'A' + remainder - 10; decimal /= 16; }*/
 	//Я знаю, что несовсем правильно, но по другому не получилось
-	cout << "Число в шестнадцатеричной системе: "; for (int i = (counter - 1); i >= 0; i--) cout << (char)hex[i];
+	/*cout << "Число в шестнадцатеричной системе: "; for (int i = (i - 1); i >= 0; i--) cout << (char)hex[i];*/
 	cout << endl;
 }
