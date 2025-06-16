2651f6227a521201cbac1718c7448b24195294ab
Илья Щербаков <iliya309@mail.ru>
2025-06-16 15:30:43 +1000

    homework 16.06

---
+++ Max2dim/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int COLS = 3;
+	const int ROWS = 3;
+	int arr[ROWS][COLS] = {
+		{1,4,8},
+		{2,9,10},
+		{3,0,6}
+	};
+
+	int max = arr[0][0];
+	int min = arr[0][0];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (arr[i][j] > max) max = arr[i][j];
+			else if (arr[i][j] < min) min = arr[i][j];
+		}
+		
+	}
+	cout <<"Максимальное значение: "<< max << endl;
+	cout <<"Минимальное значение: "<< min << endl;
+}
